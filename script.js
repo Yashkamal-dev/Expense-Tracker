97fc82037342c99a5e65ee2541709fcbc4af9ee9
Yashkamal Saraiya <252161091+Yashkamal-dev@users.noreply.github.com>
2026-02-07 16:10:49 +0530

    Refactor add transaction logic into separate module

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import { renderTransaction } from "./transactions.js";
+import { addTransaction } from "./addTransaction.js";
+
 const type = document.querySelector("#type");
 const amount = document.querySelector("#amount");
 const category = document.querySelector("#category");
@@ -15,53 +18,23 @@ if (localData === null) {
   localStorage.setItem("Transactions", JSON.stringify([]));
 }
 
-// function for new transaction input
-const addTransaction = () => {
-  let typ = type.value;
+// function to display past transaction on page load
+const pageLoad = () => {
+  let transactionArr = JSON.parse(localStorage.getItem("Transactions"));
+  transactionArr.reverse();
 
-  let amnt = Number(amount.value);
-  if (amnt == "") {
-    alert("Enter the amount.");
-    return false;
-  } else if (isNaN(amnt)) {
-    alert("Amount should only contain Numbers");
-    return false;
-  }
-
-  let ctgry = category.value;
-  if (ctgry === "-1") {
-    alert("category is required");
-    return false;
-  }
-
-  let dscrpn = description.value;
-  let dt = date.value;
-
-  console.log(dt);
-
-  let transactions = JSON.parse(localStorage.getItem("Transactions"));
-  //   console.log(transactions);
-
-  let obj = {
-    ID: `T${Date.now()}`,
-    Type: typ,
-    Amount: amnt,
-    Category: ctgry,
-    Description: dscrpn,
-    Date: dt,
-  };
-
-  console.log(obj);
-
-  //   updating for the localStorage
-  transactions.unshift(obj);
-  localStorage.setItem("Transactions", JSON.stringify(transactions));
+  transactionArr.forEach((Transaction) => {
+    renderTransaction(Transaction);
+  });
 };
 
-// on click event listener
+// invoking the function to render past transction
+pageLoad();
+
+// on add transaction button click event listener
 addBtn.addEventListener("click", addTransaction);
 
-// on enter event listener
+// on enter event listener to add transaction
 document.addEventListener("keyup", (e) => {
   if (e.key === "Enter") {
     addTransaction();
